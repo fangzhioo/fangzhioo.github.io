2ee942c1a87d0a936fb3c990c2acce43c0aa8ed5
fangzhioooo <fangzhioooo@gmail.com>
2019-04-18 14:25:53 +0800

    完成css3-features文章；添加禁止手机端双击缩放；修改google-site-verification；

--- js/in-post/css-features.js
+++ js/in-post/css-features.js
@@ -6,13 +6,13 @@ var flag1 = false;
 css_btn.onclick = function () {
     flag1 = !flag1;
     css_ele.style.height = flag1 ? "150px" : "0px";
-    css_ele.style.width = flag1 ? "150px" : "0px";
 }
 
 // JS实现 
 var js_btn = document.getElementById("transition-js-btn");
 var js_ele = document.getElementById("transition-js-ele");
-var flag2 = false, height = 0, timer;
+var flag2 = false, height = 0, timer = null;
+
 function step() {
     height = flag2 ? (height + 1) : (height - 1);
     if (height < 0) {
@@ -25,25 +25,18 @@ function step() {
         return;
     }
     js_ele.style.height = height + "px";
-    js_ele.style.width = height + "px";
-    setTimeout(step, 20);
 }
+
 js_btn.onclick = function () {
     if (timer) {
         clearInterval(timer);
     }
     flag2 = !flag2;
-    step();
+    timer = setInterval(step,1) ;
 }
 
-// // jq实现
-// var flag3 = false;
-// $("#transition-jq-btn").on("click", function () {
-//     flag3 = !flag3;
-//     if (flag3) {
-//         $("#transition-jq-ele").stop().slideDown();
-//     } else {
-//         $("#transition-jq-ele").stop().slideUp();
-//     }
-//     // $("#transition-jq-ele").stop().slideToggle();
-// })
+// jq实现
+$("#transition-jq-btn").on("click", function () {
+    $("#transition-jq-ele").stop().slideToggle();
+})
+
