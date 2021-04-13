928c01c17a57975e0c30e9f8a8f75badc131a534
fangzhi <tongren@startdt.com>
2021-04-13 17:55:49 +0800

    Create Code

---
+++ js/my-blog.js
@@ -0,0 +1,23 @@
+$(function () {
+  $("[data-toggle='tooltip']").tooltip();
+});
+
+// 头部下拉隐藏
+jQuery(document).ready(function (a) {
+  var b = 1170;
+  if (a(window).width() > b) {
+    var c = a('.navbar-custom').height();
+    a(window).on('scroll', { previousTop: 0 }, function () {
+      var b = a(window).scrollTop();
+      b < this.previousTop
+        ? b > 0 && a('.navbar-custom').hasClass('is-fixed')
+          ? a('.navbar-custom').addClass('is-visible')
+          : a('.navbar-custom').removeClass('is-visible is-fixed')
+        : (a('.navbar-custom').removeClass('is-visible'),
+          b > c &&
+            !a('.navbar-custom').hasClass('is-fixed') &&
+            a('.navbar-custom').addClass('is-fixed')),
+        (this.previousTop = b);
+    });
+  }
+});
