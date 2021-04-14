43e6284afce0058f975f8160305a9d99f24ab0c8
fangzhi <tongren@startdt.com>
2021-04-14 11:00:19 +0800

    🎉 升级hexo到5.0.0

--- themes/fangzhioo/source/js/toc.js
+++ themes/fangzhioo/source/js/toc.js
@@ -1,5 +1,5 @@
 var toc = document.getElementById('toc')
-
+console.log('tocdom', toc.classList)
 if (toc != null) {
 	window.addEventListener("scroll", scrollcatelogHandler);
 	var tocPosition = toc.offsetTop;
